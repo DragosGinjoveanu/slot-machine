9d290a7ac6be20a6351b5e952b4bc74ef2a2e8af
Ginjoveanu Dragos <82390006+DragosGinjoveanu@users.noreply.github.com>
2021-05-11 17:32:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,71 @@
 var bank = 0;
-// this function adds money to the bank
+
 function addMoneyToBank() {
   var money = parseInt(document.getElementById("budget").value);
   if (Number.isNaN(money) || money == 0) {
-    document.getElementById("status").style.color = "red";
-    document.getElementById("status").innerHTML = "Minimum deposit is $1.";
+    changeStatus("red", "Minimum deposit is $1.");
   } else {
     bank += money;
     document.getElementById("bank").innerHTML = "Bank: $" + bank;
-    document.getElementById("status").style.color = "green";
-    document.getElementById("status").innerHTML = "You added $" + money +" to your bank.";
+    changeStatus("green", "You added $" + money +" to your bank.");
   }
   return false;
 } 
-// this function generates a random character
-function randomSymbol() {
+
+function changeStatus(color, text) {
+  document.getElementById("status").style.color = color;
+  document.getElementById("status").innerHTML = text;
+}
+
+function getRandomSymbol() {
   var slots = "7/*";
   return slots[Math.floor(Math.random() * slots.length)];
 }
-// this function spins the machine
+
+//spins the machine
 function playGame() {
   if (bank == 0) {
-      document.getElementById("status").style.color = "red";
-      document.getElementById("status").innerHTML = "You ran out of money!";
+      changeStatus("red", "You ran out of money!");
   } else {
       var bet = parseInt(document.getElementById("bet").value);
       if (bet > bank) {
-        document.getElementById("status").style.color = "red";
-        document.getElementById("status").innerHTML = "You don't have enough money!";
+        changeStatus("red", "You don't have enough money!");
       } else if (bet <= 0 || Number.isNaN(bet)) {
-        document.getElementById("status").style.color = "red";
-        document.getElementById("status").innerHTML = "Minimum bet is $1.";
+        changeStatus("red", "Minimum bet is $1.");
       } else {
         bank -= bet;
         document.getElementById("bank").innerHTML = "Bank: $" + bank;
-        var randomCharacter1 = randomSymbol();
-        var randomCharacter2 = randomSymbol();
-        var randomCharacter3 = randomSymbol();
+        var randomCharacter = [];
+        for (var i = 1; i <= 3; i++) {
+          randomCharacter[i] = getRandomSymbol();
+        }
         document.getElementById("slotMachine").innerHTML = "";
-        document.getElementById("slotMachine").innerHTML = randomCharacter1 + " " + randomCharacter2 + " " + randomCharacter3;
-        gameStatus(randomCharacter1, randomCharacter2, randomCharacter3, bet);
+        document.getElementById("slotMachine").innerHTML = randomCharacter[1] + " " + randomCharacter[2] + " " + randomCharacter[3];
+        checkGameStatus(randomCharacter, bet);
       }
   }
   return false;
 }
-// this function tells you if you won/lost and updates the bank
-function gameStatus(symbol1, symbol2, symbol3, bet) {
-  if (symbol1 == symbol2 && symbol2 == symbol3) {
+
+//tells you if you won/lost and updates the bank
+function checkGameStatus(slots, bet) {
+  if (slots[1] == slots[2] && slots[2] == slots[3]) {
       var money;
-      document.getElementById("status").style.color = "green";
-      if (symbol1 == '7') {
+      if (slots[1] == '7') {
         money = bet * 5;
-      } else if (symbol1 == '*') {
+      } else if (slots[1] == '*') {
         money = bet * 3;
-      } else if (symbol = '/') {
+      } else if (slots[1] = '/') {
         money = bet * 2;
       }
       bank += money;
-      document.getElementById("status").innerHTML = "You won $" + money + "!";
+      changeStatus("green", "You won $" + money + "!");
       document.getElementById("bank").innerHTML = "Bank: $" + bank;
   } else {
-      document.getElementById("status").style.color = "red";
-      document.getElementById("status").innerHTML = "You lost!";
+      changeStatus("red", "You lost!");
   }
 }
-// this function restarts the whole game
+
 function restart() {
   location.reload();
 }
